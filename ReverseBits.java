9fba3cdc6eb65afbda40ee6efa6d6611c6f7d065
qbieber <qbieber@hotmail.com>
2021-10-21 12:32:15 -0400

    Reverse Bits leetcode solution

---
+++ ReverseBits.java
@@ -0,0 +1,9 @@
+class ReverseBits {
+    public static int reverseBits(int n) {
+        return Integer.reverse(n);
+    }
+
+    public static void main(String[] args) {
+        System.out.print(reverseBits(8));
+    }
+}
